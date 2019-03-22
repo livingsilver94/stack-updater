1f6e8cdf3d53b1cf64eecf65e0c4dfbf216b0c2a
Fabio Forni <fabio@redaril.me>
2019-03-22 12:07:53 +0100

    Adapt KDE stack to the new PageBody & simplify code

--- stack/kde/kde.go
+++ stack/kde/kde.go
@@ -4,12 +4,12 @@ import (
 	"fmt"
 	"github.com/livingsilver94/stack_updater/stack"
 	"golang.org/x/net/html"
-	"io"
-	"io/ioutil"
 	"strings"
 )
 
 const BaseURL = "https://cdn.download.kde.org/stable"
+
+
 
 type KDEStack struct {
 	Bundle  string
@@ -22,37 +22,23 @@ func (kde KDEStack) FetchPackages() (map[string]string, error) {
 	return nil, nil
 }
 
-func (KDEStack) ParsePage(page io.Reader) ([]string, error) {
+func (KDEStack) ParsePage(page []byte) ([]string, error) {
 	pkgList := make([]string, 0, 20)
-	var pageString string
-	if pageData, err := ioutil.ReadAll(page); err != nil {
-		return pkgList, err
-	} else {
-		pageString = string(pageData)
-	}
-	pageString = enclosedString(pageString, "<ul>", "</ul>")
+	pageString := enclosedString(string(page), "<ul>", "</ul>")
 	tokenizer := html.NewTokenizer(strings.NewReader(pageString))
 	// We are parsing lines like <li><a href="filename"> filename</a></li>
-loop:
-	for {
-		switch tokenizer.Next() {
-		case html.StartTagToken:
-			{
-				tokenizer.Next()
-				tokenizer.Next()
-				token := tokenizer.Token()
-				pkgList = append(pkgList, token.Data)
-			}
-		case html.ErrorToken:
-			{
-				break loop
-			}
-		}
+    tokenType := tokenizer.Next()
+	for ;tokenType != html.ErrorToken; tokenType = tokenizer.Next() {
+        if tokenType == html.StartTagToken {
+            tokenizer.Next(); tokenizer.Next()
+            token := tokenizer.Token()
+			pkgList = append(pkgList, strings.Trim(token.Data, " "))
+        }
 	}
 	return pkgList, nil
 }
 
-func (kde KDEStack) packagesPage() (io.Reader, error) {
+func (kde KDEStack) packagesPage() ([]byte, error) {
 	fullURL := fmt.Sprintf("%s/%s/%s/src", BaseURL, kde.Bundle, kde.Version)
 	if page, err := stack.PageBody(fullURL); err == nil {
 		return page, nil
