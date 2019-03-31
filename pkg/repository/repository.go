f10e5ad4a459466b081dbcbc291f330f889c2b8a
Fabio Forni <fabio@redaril.me>
2019-03-31 21:02:28 +0200

    Move packageSource struct into its own file

--- pkg/repository/repository.go
+++ pkg/repository/repository.go
@@ -2,83 +2,17 @@ package repository
 
 import (
 	"encoding/xml"
-	"gopkg.in/libgit2/git2go.v26"
-	"io/ioutil"
 	"os"
 	"sort"
 	"path/filepath"
-	"regexp"
-	"strings"
+	"gopkg.in/libgit2/git2go.v26"
+	"io/ioutil"
 )
 
 const (
 	Filepath          = "/var/lib/eopkg/index/Solus/eopkg-index.xml"
 	SourceBaseURL     = "https://dev.getsol.us/source/"
-	PkgDefinitionFile = "package.yml"
 )
-
-// This is a ugly yet necessary hack since we don't have a package.yml linter *yet*
-// A package.yml file in fact is not a fully YAML-compliant file and we cannot parse it as a YAML
-var yamlEditor = regexp.MustCompile(`^(?P<key>[[:alnum:]]+)(?P<separator> *: *)(?P<value>[[:print:]]+)$`)
-
-type packageSource struct {
-	path string
-	definition string
-}
-
-func newPackageSource(path string) (*packageSource, error) {
-	defFile, err := ioutil.ReadFile(filepath.Join(path, PkgDefinitionFile))
-	if err == nil {
-		return &packageSource{path, string(defFile)}, err
-	}
-	return nil, err
-}
-
-func (source *packageSource) Entry(key string) string {
-	for _, line := range source.readDefLines() {
-		if matches := yamlEditor.FindStringSubmatch(line); matches != nil && matches[1] == key {
-			return matches[3]
-		}
-	}
-	return ""
-}
-
-func (source *packageSource) UpdateEntry(key, value string) error {
-	var builder strings.Builder
-	for _, line := range source.readDefLines() {
-		if matches := yamlEditor.FindStringSubmatch(line); matches != nil && matches[1] == key {
-			line = yamlEditor.ReplaceAllString(line, "${key}${separator}" + value)
-		}
-		builder.WriteString(line+"\n")
-	}
-	source.definition = builder.String()
-	return source.writeDefFile()
-}
-
-func (source *packageSource) UpdateEntries(defs map[string]string) error {
-	var builder strings.Builder
-	for _, line := range source.readDefLines() {
-		for key, value := range defs {
-			if matches := yamlEditor.FindStringSubmatch(line); matches != nil && matches[1] == key {
-				line = yamlEditor.ReplaceAllString(line, "${key}${separator}" + value)
-				break
-			}
-		}
-		builder.WriteString(line+"\n")
-	}
-	source.definition = builder.String()
-	return source.writeDefFile()
-}
-
-func (source *packageSource) writeDefFile() error {
-	return ioutil.WriteFile(filepath.Join(source.path, PkgDefinitionFile), []byte(source.definition), 0644)
-}
-
-func (source *packageSource) readDefLines() []string {
-	return strings.Split(source.definition, "\n")
-}
-
-
 
 type update struct {
 	version string `xml:"Version"`
