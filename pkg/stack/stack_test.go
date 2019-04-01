98c94b5e18106af8c41885f40a50f753473118f1
Fabio Forni <fabio@redaril.me>
2019-04-02 01:31:04 +0200

    Add test file for stack.go

---
+++ pkg/stack/stack_test.go
@@ -0,0 +1,24 @@
+package stack_test
+
+import (
+	"testing"
+	"github.com/livingsilver94/stack-updater/pkg/stack"
+)
+
+func TestPackageFromFilename(t *testing.T) {
+	url := "http://test.com"
+	var tests = []struct {
+		in string
+		out        stack.Package
+	}{
+		{"this-is-a-123-TEST-1.1.zip", stack.Package{"this-is-a-123-TEST", "1.1", url}},
+		{"this-is-a-123-TEST-1.1.tar.gz", stack.Package{"this-is-a-123-TEST", "1.1", url}},
+		{"thisisnotvalid.zip", stack.Package{}},
+		{"fooFoo-9.99.java.tar", stack.Package{"fooFoo", "9.99", url}},
+	}
+	for _, testVal := range tests {
+		if ret, _ := stack.PackageFromFilename(testVal.in, url); ret != testVal.out {
+			t.Errorf("in: %s\texpected: %v\tgot: %v", testVal.in, testVal.out, ret)
+		}
+	}
+}
