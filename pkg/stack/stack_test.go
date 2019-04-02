5762de18ae5c6bc2f9a86fbea5dbe7555898a63a
Fabio Forni <fabio@redaril.me>
2019-04-02 13:45:24 +0200

    Improve stack_test.go readability

--- pkg/stack/stack_test.go
+++ pkg/stack/stack_test.go
@@ -1,16 +1,18 @@
 package stack_test
 
 import (
-	"testing"
 	"github.com/livingsilver94/stack-updater/pkg/stack"
+	"net/http/httptest"
+	"testing"
 )
 
 func TestPackageFromFilename(t *testing.T) {
+	type Exp struct {
+		in  string
+		out stack.Package
+	}
 	url := "http://test.com"
-	var tests = []struct {
-		in string
-		out        stack.Package
-	}{
+	var tests = []Exp{
 		{"this-is-a-123-TEST-1.1.zip", stack.Package{"this-is-a-123-TEST", "1.1", url}},
 		{"this-is-a-123-TEST-1.1.tar.gz", stack.Package{"this-is-a-123-TEST", "1.1", url}},
 		{"thisisnotvalid.zip", stack.Package{}},
