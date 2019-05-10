1b31cb73ccd7174edc48b5df172bed77ff9481f9
Fabio Forni <fabio@redaril.me>
2019-05-11 01:06:51 +0200

    Set --version flag

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -4,7 +4,10 @@ import (
 	"github.com/spf13/cobra"
 )
 
+// RootCmd is the parent command. It stores application's
+// information and works as a dispatcher for subcommands.
 var RootCmd = &cobra.Command{
-	Use:   "stack-updater",
-	Short: "A stack update helper for Solus",
+	Use:     "stack-updater",
+	Short:   "A stack update helper for Solus",
+	Version: "1.0.0",
 }
