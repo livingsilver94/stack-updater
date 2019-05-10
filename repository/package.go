cd59a9f79d097550547d3f4eb84d542ae1f2625a
Fabio Forni <fabio@redaril.me>
2019-05-10 20:39:43 +0200

    package.go: better docstrings

--- repository/package.go
+++ repository/package.go
@@ -8,29 +8,27 @@ import (
 )
 
 const (
-	// SourceBaseURL is a base URL from which to download package sources
+	// SourceBaseURL is a base URL from which to download package sources.
 	SourceBaseURL = "https://dev.getsol.us/source/"
 )
 
-// Update represent a commit of a Solus package. Every new release is
-// distinguished by Release
+// Update represent a commit of a Solus package.
 type Update struct {
+	// Upstream version
 	Version string `xml:"Version"`
+	// Solus package release, incremental.
 	Release string `xml:"release,attr"`
 }
 
-// Package represents a software package inside the Solus repository
+// Package represents a software package inside the Solus repository.
 type Package struct {
 	Name    string `xml:"Name"`
 	Source  *packageSource
 	Updates []Update `xml:"History>Update"`
 }
 
-// DownloadSources downloads this package's source files to directory.
-// It clones a git repository so that it's possible to manually
-// browse into the directory and perform usual git operations.
-//
-// DownloadSources also populate Package.Source field
+// DownloadSources clones this package's git repository to directory
+// and populates Package.Source field.
 func (pkg *Package) DownloadSources(directory string) error {
 	sourcePath := filepath.Join(directory, pkg.Name)
 	if _, err := os.Stat(filepath.Join(sourcePath, PkgDefinitionFile)); os.IsNotExist(err) {
@@ -47,7 +45,7 @@ func (pkg *Package) DownloadSources(directory string) error {
 	return nil
 }
 
-// CurrentVersion returns package's latest version available in the repository
+// CurrentVersion returns package's latest version available in the repository.
 func (pkg *Package) CurrentVersion() string {
 	return pkg.Updates[0].Version
 }
